8a5059b661f956dfcc238977b70e1781bfa6e406
Vinicius Lage <vinicius_clage@yahoo.com.br>
2025-10-01 09:40:14 -0300

    feat: configure metrics middleware using prombundle

--- services/orders-service/app.js
+++ services/orders-service/app.js
@@ -4,11 +4,26 @@ const { trace, context } = require("@opentelemetry/api");
 const pino = require("pino");
 const pinoHttp = require("pino-http");
 const ecsFormat = require("@elastic/ecs-pino-format");
+const promBundle = require('express-prom-bundle');
 
 const logger = pino({
   level: "info",
   ...ecsFormat(),
   base: null,
+});
+
+const metricsMiddleware = promBundle({
+  includeMethod: true,
+  includePath: true,
+  includeStatusCode: true,
+  autoregister: true,
+  promClient: {
+    collectDefaultMetrics: {},
+  },
+  normalizePath: [
+    [/^\/metrics$/, '/metrics'], 
+    [/^\/order\/[^\/]+$/, '/order/:id']
+  ],
 });
 
 const loggingMiddleware = pinoHttp({
@@ -38,11 +53,17 @@ const loggingMiddleware = pinoHttp({
 });
 
 const app = express();
+app.use(metricsMiddleware);
 app.use(loggingMiddleware);
 
 app.get("/order/:id", (req, res) => {
   req.log.info({ msg: "Fetching order", orderId: req.params.id });
   res.json({ orderId: req.params.id, status: "confirmed" });
+});
+
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', metricsMiddleware.promClient.register.contentType);
+  res.send(metricsMiddleware.promClient.register.metrics());
 });
 
 app.listen(3000, () => {

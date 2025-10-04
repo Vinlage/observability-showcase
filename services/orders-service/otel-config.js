47d515b28d05a7178b363562604e7f3a413f1f7b
Vinicius Lage <vinicius_clage@yahoo.com.br>
2025-10-04 15:38:51 -0300

    refactor: remove prom bundle to use otel with prometheus

--- services/orders-service/otel-config.js
+++ services/orders-service/otel-config.js
@@ -1,11 +1,28 @@
-const opentelemetry = require("@opentelemetry/sdk-node");
-const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
-const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-http");
-const { OTLPMetricExporter } = require("@opentelemetry/exporter-metrics-otlp-http");
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { resourceFromAttributes  } from "@opentelemetry/resources";
+import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from "@opentelemetry/semantic-conventions";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
+import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 
-const sdk = new opentelemetry.NodeSDK({
-  traceExporter: new OTLPTraceExporter({ url: "http://jaeger:4318/v1/traces" }),
-  instrumentations: [getNodeAutoInstrumentations()],
+const sdk = new NodeSDK({
+  resource: resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: "orders-service",
+    [ATTR_SERVICE_VERSION]: "1.0.0",
+  }),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter({ url: "http://otel-collector:4318/v1/metrics" }),
+    exportIntervalMillis: 5000,
+  }),
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      '@opentelemetry/instrumentation-runtime-node': {
+        enabled: true,
+      },
+    }),
+    new HttpInstrumentation(),
+  ],
 });
 
 sdk.start();
